f3e0fb3384ea8bbf904cdfce1c4d339b0eba69da
VinyciusDEV <viny.antunes@gmail.com>
2023-01-18 20:06:46 -0300

    Adicionando [HTTPGet] e trocando List<> por IEnumerable

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -10,11 +10,19 @@ public class FilmeController : ControllerBase
 
     private static List<Filme> filmes = new List<Filme>();
 
+    //inserção de filmes
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)
     {
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
+    }
+
+    //lista dos filmes
+    [HttpGet]
+    public List<Filme> RecuperaFilmes()
+    {
+        return filmes;
     }
 }
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -12,7 +12,7 @@ public class Filme
     [Range(1, 360, ErrorMessage = "A duração deve ter no mínimo 1 minuto e no máximo 360")]
     public int Duracao { get; set; }
 
-    [Required(ErrorMessage = "O Diretor é obrigátório")]
+    
     [MaxLength(200, ErrorMessage = "O Diretor não pode ultrapassar 400 caracteres")]
     public string Diretor { get; set; }
 

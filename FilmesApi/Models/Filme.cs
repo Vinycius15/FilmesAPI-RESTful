9ef16e2cb78e7532ea2a67d0bdfc56b461d72ae4
VinyciusDEV <viny.antunes@gmail.com>
2023-01-18 18:26:43 -0300

    first commit

---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O título do filme não pode exceder 50 caracteres")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O campo de duração é obrigatório")]
+    [Range(1, 360, ErrorMessage = "A duração deve ter no mínimo 1 minuto e no máximo 360")]
+    public int Duracao { get; set; }
+
+    [Required(ErrorMessage = "O Diretor é obrigátório")]
+    [MaxLength(200, ErrorMessage = "O Diretor não pode ultrapassar 400 caracteres")]
+    public string Diretor { get; set; }
+
+    [Required(ErrorMessage = "O genêro é obrigátório")]
+    [MaxLength(200, ErrorMessage = "O genêro não pode ultrapassar 200 caracteres")]
+    public string Genero { get; set; }
+}

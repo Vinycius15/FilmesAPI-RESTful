3fa21b25bf8bc7374fc810e613b9be2767852586
VinyciusDEV <viny.antunes@gmail.com>
2023-01-20 00:51:15 -0300

    Deixando o código mais REST; mostrar not found quando o filme não existe; retornar os dados do filme ao usuário quando cadastrar

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -14,9 +14,6 @@ public class Filme
     [Range(1, 360, ErrorMessage = "A duração deve ter no mínimo 1 minuto e no máximo 360")]
     public int Duracao { get; set; }
 
-    
-    public string Diretor { get; set; }
-
     [Required(ErrorMessage = "O genêro é obrigátório")]
     [MaxLength(200, ErrorMessage = "O genêro não pode ultrapassar 200 caracteres")]
     public string Genero { get; set; }

d6988f4f7e45e047de45e6f5bc529daa7104877c
VinyciusDEV <viny.antunes@gmail.com>
2023-01-25 20:25:19 -0300

    Terminando ajustes para integração com banco de dados

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesApi.Models;
+﻿using FilmesApi.Data;
+using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
@@ -8,15 +9,20 @@ namespace FilmesApi.Controllers;
 public class FilmeController : ControllerBase
 {
 
-    private static List<Filme> filmes = new List<Filme>();
-    private static int id = 0;
+    private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
+      
 
 
     [HttpPost]
     public IActionResult IActionResult ([FromBody] Filme filme)
     {
-        filme.Id = id++;
-        filmes.Add(filme);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
         return CreatedAtAction(nameof(RecuperaFilmePorId), 
                                new { id = filme.Id }, 
                                filme);
@@ -26,14 +32,14 @@ public class FilmeController : ControllerBase
     public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, 
                                              [FromQuery] int take=50 )
     {
-        return filmes.Skip(skip).Take(take);
+        return _context.Filmes.Skip(skip).Take(take);
     }
 
     // filme especifico
     [HttpGet("{Id}")]
     public IActionResult RecuperaFilmePorId(int id)
     {
-        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
         if (filme == null) return NotFound();
         return Ok(filme);
     }
